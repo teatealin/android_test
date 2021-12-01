bb1ceebed68b791526837af81e76a2adbab461a1
lry <287874075@qq.com>
2021-12-01 22:52:03 +0800

    增加相关demo以及相关书籍

--- app/src/main/java/com/example/android_test/MainActivity.kt
+++ app/src/main/java/com/example/android_test/MainActivity.kt
@@ -2,10 +2,16 @@ package com.example.android_test
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MotionEvent
+import android.view.View
+import android.view.View.OnTouchListener
+import android.widget.FrameLayout
+import android.app.Activity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        
     }
 }
